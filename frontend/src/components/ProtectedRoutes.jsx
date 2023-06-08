3ac0ee2a1f1ab3a1ab38305437b94b7448ed3095
Safdar Hussain <safdarhussainmangnejo@gmail.com>
2023-06-09 00:49:00 +0500

    updated folder

--- frontend/src/components/ProtectedRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom"
-
-const ProtectedRoute = () => {
-    
-    const auth = localStorage.getItem('token');
-
-    return auth ? <Outlet /> : <Navigate to="SignIn" />
-}
-export default ProtectedRoute;
